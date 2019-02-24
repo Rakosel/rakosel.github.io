314fbd0652b17e4c390fb0244b223e2d5b4a9c9e
Rakosel <47699273+Rakosel@users.noreply.github.com>
2019-02-24 05:37:55 +0400

    Update wsb_script_2_2_1.js

--- wsb_script_2_2_1.js
+++ wsb_script_2_2_1.js
@@ -1,4 +1,4 @@
-  //upd24a 	https://rakosel.github.io/wsb_script_2_2_1.js  otkl timer
+  //upd25 	https://rakosel.github.io/wsb_script_2_2_1.js  otkl timer
   // #176		add btn_lm75_1s()
   // #44 dorabotka uart_submit + checkbox with ua fetch		
 		var stopAll = false, ra, rs, submitted = false, lines_in, i, url;
@@ -57,7 +57,7 @@
   			{
   				uart_json.uart_in = lines_in[i];
 					if(ua_mode==1)
-					{fetch('/uart_get?input=' + encodeURIComponent(lines_in[i])+'&', 'GET', txjs_ua, 30);alert("uart "+ua_mode);}
+					{fetch('/uart_get?input=' + encodeURIComponent(lines_in[i])+'&', 'GET', txjs_ua, 30);}
 					else
 					{fetch('/uart.json?n=' + encodeURIComponent(JSON.stringify(uart_json))+'&', 'GET', txjs_ua, 30);}						
   			}
