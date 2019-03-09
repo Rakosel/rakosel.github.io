04e91260a83484ad490808518184808168e0d0c0
Rakosel <47699273+Rakosel@users.noreply.github.com>
2019-03-09 23:18:19 +0400

    Update wsb_script_2_2_1.js

--- wsb_script_2_2_1.js
+++ wsb_script_2_2_1.js
@@ -1,4 +1,4 @@
-//upd53b5 with upravl timer	https://rakosel.github.io/wsb_script_2_2_1.js  
+//upd53b6 with upravl timer	https://rakosel.github.io/wsb_script_2_2_1.js  
 // #84 dorabotal 'ACK' otkl autoload
 // #303 vopros pro wide 	
 // #416 391
@@ -33,12 +33,15 @@
 			i=0;
 			cnfOBJ = $("#scntf").serializeArray(); 
 			//console.log(cnfOBJ);
+			//$('input:checkbox:checked').each(function(){
+    			//		alert($(this).val());
+			//});
 			
 			for(i=0;i<10;i++)
 			{
 				if($("#gLM75ch" + i))
 				{console.log("true "+i); }
-				console.log($("#gLM75ch" + i));
+				console.log($("#gLM75ch" + i).val());
 			}
 		}
 
