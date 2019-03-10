c93ce77b70495612681c8f926abcd11a720fa115
Rakosel <47699273+Rakosel@users.noreply.github.com>
2019-03-10 21:01:21 +0400

    Update wsb_script_2_2_1.js

--- wsb_script_2_2_1.js
+++ wsb_script_2_2_1.js
@@ -1,4 +1,4 @@
-//upd54d with upravl timer	https://rakosel.github.io/wsb_script_2_2_1.js  
+//upd54c1 with upravl timer	https://rakosel.github.io/wsb_script_2_2_1.js  
 // #84 dorabotal 'ACK' otkl autoload
 // #303 vopros pro wide 	
 // #416 391
@@ -70,7 +70,7 @@
 			//});
 			//input_lm75.input_lm75_1[0] = 1;
 			
-			for(i=0;(i < 5) && ($("#gLM75ch" + i).val()!=undefined);++i )
+			for(i=0;(i < 4) && ($("#gLM75ch" + i).val()!=undefined);++i )
 			{
 				if($("#gLM75ch" + i).prop("checked"))
 				{tarch|=(1 << i);}
