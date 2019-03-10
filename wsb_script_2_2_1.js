13c9889fdbd99c0ab4d620be63f9ac645238d5c9
Rakosel <47699273+Rakosel@users.noreply.github.com>
2019-03-10 04:25:40 +0400

    Update wsb_script_2_2_1.js

--- wsb_script_2_2_1.js
+++ wsb_script_2_2_1.js
@@ -42,13 +42,20 @@
 			thyst1 = cnfOBJ[0].value;
 			if(tos1==thyst1)
 			{thyst1++;tos1--;}
-			if(thyst1>tos1)
-			{$("#"+cnfOBJ[0].name).val(tos1);$("#"+cnfOBJ[1].name).val(thyst1);
+			if(thyst1<tos1 && (thyst1<0) && (thyst1<0))
+			{
+				$("#"+cnfOBJ[0].name).val(tos1);$("#"+cnfOBJ[1].name).val(thyst1);
 				cnfOBJ[1].value = thyst1;
 				cnfOBJ[0].value = tos1;
+			}
+			else if(thyst1>tos1)
+			{
+				$("#"+cnfOBJ[0].name).val(tos1);$("#"+cnfOBJ[1].name).val(thyst1);
+				cnfOBJ[1].value = thyst1;
+				cnfOBJ[0].value = tos1;
+			}
 			  $('.lm75thy_1').text($("#"+cnfOBJ[0].name).val()+" C°");
 			  $('.lm75tos_1').text($("#"+cnfOBJ[1].name).val()+" C°");
-			}
 			//console.log(cnfOBJ[0].value+" "+ cnfOBJ[1].value);
 			//$('input:checkbox:checked').each(function(){
 			//});
