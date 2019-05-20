cbdeda10602af70ee163b33f5666a03e931c2923
Rakosel <47699273+Rakosel@users.noreply.github.com>
2019-05-21 01:29:14 +0300

    Update wsb_script_2_2_1.js

--- wsb_script_2_2_1.js
+++ wsb_script_2_2_1.js
@@ -1,4 +1,4 @@
-// upd71cd with upravl timer	https://rakosel.github.io/wsb_script_2_2_1.js  
+// upd71d with upravl timer	https://rakosel.github.io/wsb_script_2_2_1.js  
 // http://qaru.site/questions/66646/how-to-recognize-touch-events-using-jquery-in-safari-for-ipad-is-it-possible
 // function time(){
 // return parseInt(new Date().getTime()/1000)
@@ -281,11 +281,11 @@ if(parseInt(temp_json.lm75_1_ou[1], 10) != 999 || parseInt(temp_json.lm75_1_ou[0
 			//$("#bm1_ch").val("0x"+Number(temp_json.bme280_1_ou[0]).toString(16).toUpperCase());
 				var aou1 = parseInt(temp_json.lm75_1_ou[1], 10);
 				var aou2 = parseInt(temp_json.lm75_1_ou[2], 10);
-				aou1 = Math.round(aou1/2);
-				aou2 = Math.round(aou1/2);
+				aou1 = aou1>>1;
+				aou2 = aou2>>1;
 			
-				$('.lm75thy_1').text(aou1+"°C");
-				$('.lm75tos_1').text(aou2+"°C");
+				$('.lm75thy_1').text(aou1+" °C");
+				$('.lm75tos_1').text(aou2+" °C");
 				$("#lm75sc_1").val(aou1);
 				$("#lm75so_1").val(aou2);
 
@@ -321,8 +321,8 @@ if(parseInt(temp_json.lm75_2_ou[1], 10) != 999 || parseInt(temp_json.lm75_2_ou[0
 			//$("#bm1_ch").val("0x"+Number(temp_json.bme280_1_ou[0]).toString(16).toUpperCase());
 				var aou1 = parseInt(temp_json.lm75_2_ou[1], 10);
 				var aou2 = parseInt(temp_json.lm75_2_ou[2], 10);
-				aou1 = Math.round(aou1/2);
-				aou2 = Math.round(aou1/2);
+				aou1 = aou1>>1;
+				aou2 = aou2>>1;
 			
 				$('.lm75thy_2').text(aou1+"°C");
 				$('.lm75tos_2').text(aou2+"°C");
