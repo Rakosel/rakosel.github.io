c63ef0a8e213436d9ce5953af3f4f38145fde837
Rakosel <47699273+Rakosel@users.noreply.github.com>
2019-05-20 22:16:17 +0300

    Update wsb_script_2_2_1.js

--- wsb_script_2_2_1.js
+++ wsb_script_2_2_1.js
@@ -1,4 +1,4 @@
-// upd71a with upravl timer	https://rakosel.github.io/wsb_script_2_2_1.js  
+// upd71aa with upravl timer	https://rakosel.github.io/wsb_script_2_2_1.js  
 // http://qaru.site/questions/66646/how-to-recognize-touch-events-using-jquery-in-safari-for-ipad-is-it-possible
 // function time(){
 // return parseInt(new Date().getTime()/1000)
@@ -315,7 +315,7 @@ if(parseInt(temp_json.lm75_2_ou[1], 10) != 999 || parseInt(temp_json.lm75_2_ou[0
 				{
 			//$("#bm1_ch").val("0x"+Number(temp_json.bme280_1_ou[0]).toString(16).toUpperCase());
 				$('.lm75thy_2').text(parseInt(temp_json.lm75_2_ou[1], 10));
-				$('.lm75tos_2').text(parseInt(temp_json.lm75_2_ou[2], 10);
+				$('.lm75tos_2').text(parseInt(temp_json.lm75_2_ou[2], 10));
 				$("#lm75sc_2").val(parseInt(temp_json.lm75_2_ou[1], 10));
 				$("#lm75so_2").val(parseInt(temp_json.lm75_2_ou[2], 10));
 
